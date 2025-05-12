f8e553c12ad2b12b5b768e84f7a40f5fde75ecf6
iznurfarahin <iznurnain0092@gmail.com>
2025-05-13 07:29:35 +0800

    Delete Portfolio website/Portfolio website directory

--- Portfolio website/Portfolio website/js/animation.js
+++
@@ -1,7 +0,0 @@
-/*=========== typing animation */
-var typed = new Typed(".typing", { //line 49
-  strings: ["", "Web Developer", "Web Designer", "Graphic Designer"],
-  typeSpeed: 100,
-  BackSpeed: 60,
-  loop: true
-})
